2c0e21302ef0d82d6cd1157910ee14c5c0eeb95c
yaosabi <lorenzoimurillo@gmail.com>
2023-09-28 14:27:02 -0700

    worked on stuff

--- app/src/main/java/com/example/myapplication/ui/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/example/myapplication/ui/dashboard/DashboardViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class DashboardViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is where the local events will be"
     }
     val text: LiveData<String> = _text
 }
